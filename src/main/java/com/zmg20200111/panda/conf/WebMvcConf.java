7ae6ef619314a20a89b879fc1129db00717a89c5
Andy12138 <1208799079@qq.com>
2020-04-15 15:13:08 +0800

    删除老的websocket代码

---
+++ src/main/java/com/zmg20200111/panda/conf/WebMvcConf.java
@@ -0,0 +1,24 @@
+package com.zmg20200111.panda.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Andy
+ */
+@Configuration
+public class WebMvcConf implements WebMvcConfigurer {
+
+    /**
+     * 配置跨域请求处理
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "put", "DELETE");
+    }
+}
